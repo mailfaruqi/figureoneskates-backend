a7eedbc8ebc28e0a86f5be7e16b14976ef2e5955
mailfaruqee <ismailfaruqi7@gmail.com>
2024-06-21 19:39:57 +0700

    feat: example to seed

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -14,9 +14,19 @@ export const dataProducts = [
     name: "Edea Overture", // unique
     imageURL:
       "https://cdn.shopify.com/s/files/1/0593/2593/9907/products/overture_black_blade_2_1_1_1_1_grande.jpg?v=1638854943",
-    price: 10000000,
+    price: 12000000,
     description:
       "This skate is ideal for skaters landing single jumps and starting doubles",
     sku: "T01XXX000003", // unique
+  },
+  {
+    slug: "risport-electra", // unique
+    name: "Risport Electra", // unique
+    imageURL:
+      "https://skatingboutique.com/cdn/shop/products/29a789803c1251ea8946b003322e86f9_b93460cf-4aed-4c9b-8a06-df3627e6e6a4_1024x1024.jpg?v=1677596164",
+    price: 10000000,
+    description:
+      "This skate is ideal for skaters landing single jumps and starting doubles",
+    sku: "C12XXX002987", // unique
   },
 ];
